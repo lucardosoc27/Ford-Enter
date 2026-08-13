94f85151f0ab0989b47d68a1d5909edc8a87b660
Luã <lucardosoc27@gmail.com>
2026-08-13 14:30:32 -0300

    Desafio da Sprint JS

--- anexo_desafio_htmlcss/js/carousel.js
+++ anexo_desafio_htmlcss/js/carousel.js
@@ -3,10 +3,12 @@ let carouselArr = [];
 
 // classe
 class Carousel {
+
   // atributo estatico
   static _sequence = 0;
   static _size = 0;
   static _interval = null;
+  static _arr = [];
 
   // atributos da instancia da classe
   constructor(image, title, url) {
@@ -15,37 +17,47 @@ class Carousel {
     this.Url = url;
   }
 
-  // metodo estatico
+  // metodo estatico da classe
   static Start(arr) {
-    if (arr) {
-      if (arr.length > 0) {
+      if (arr && arr.length > 0) {
         Carousel._sequence = 0;
+        Carousel._arr = arr;
         Carousel._size = arr.length;
-        Carousel.Next();
-        Carousel._interval = setInterval(function () {
-          Carousel.Next();
-        }, 5000);
-      }
+        Carousel.Atualizar();
+        Carousel._interval = setInterval(() => Carousel.Next(), 2000);
     } else {
       throw "Method Start need a Array Variable.";
     }
   }
 
+  // metodo pra avançar pro próximo slide
   static Next() {
-    let slide = carouselArr[Carousel._sequence];
-
-    // troca a imagem do carousel pelo js
-    let carouselDiv = document.getElementById("carousel");
-    carouselDiv.style.backgroundImage = `url('anexo_desafio_htmlcss/img/${slide.Image}')`;
-    carouselDiv.style.backgroundSize = "cover";
-    carouselDiv.style.backgroundPosition = "center";
-
-    let tituloDiv = document.getElementById("carousel-title");
-    tituloDiv.innerHTML = `<a href='${slide.Url}'>${slide.Title}</a>`;
-
     Carousel._sequence++;
     if (Carousel._sequence >= Carousel._size) {
       Carousel._sequence = 0;
     }
+    Carousel.Atualizar();
+  }
+  
+  // metodo pra voltar pro slide anterior
+  static Previous () {
+    Carousel._sequence--;
+    if (Carousel._sequence < 0) {
+      Carousel._sequence = Carousel._size -1;
+    }
+    Carousel.Atualizar();
+  }
+
+  // exibir o slide na posição atual na tela
+  static Atualizar() {
+    const slide = Carousel._arr[Carousel._sequence];
+
+    const carouselDiv = document.getElementById("carousel");
+    carouselDiv.style.backgroundImage = `url('anexo_desafio_htmlcss/img/${slide.Image}')`;
+    carouselDiv.style.backgroundSize = "cover";
+    carouselDiv.style.backgroundPosition = "center";
+
+    const tituloDiv = document.getElementById("carousel-title");
+    tituloDiv.innerHTML = `<a href='${slide.Url}'>${slide.Title}</a>`;
   }
 }
--- anexo_desafio_htmlcss/js/form.js
+++ anexo_desafio_htmlcss/js/form.js
@@ -1,46 +1,49 @@
 class Contato {
-
-    constructor(nome, sobrenome, email, cpf, telefone, tipo, mensagem) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.email = email;
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.tipo = tipo;
-        this.mensagem = mensagem;
-    }
+  constructor(nome, sobrenome, email, cpf, telefone, tipo, mensagem) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.email = email;
+    this.cpf = cpf;
+    this.telefone = telefone;
+    this.tipo = tipo;
+    this.mensagem = mensagem;
+  }
 }
 
-function MostrarDadosConsole (form) {
-    console.log({
-        nome: form.elements.namedItem("nome").value,
-        sobrenome: form.elements.namedItem("sobrenome").value,
-        email: form.elements.namedItem("email").value,
-        cpf: form.elements.namedItem("cpf").value,
-        telefone: form.elements.namedItem("telefone").value,
-        tipo: form.elements.namedItem("tipo").value,
-        mensagem: form.elements.namedItem("mensagem").value
-    });
+function MostrarDadosConsole(form) {
+  console.log({
+    nome: form.elements.namedItem("nome").value,
+    sobrenome: form.elements.namedItem("sobrenome").value,
+    email: form.elements.namedItem("email").value,
+    cpf: form.elements.namedItem("cpf").value,
+    telefone: form.elements.namedItem("telefone").value,
+    tipo: form.elements.namedItem("tipo").value,
+    mensagem: form.elements.namedItem("mensagem").value,
+  });
 }
 
 function Post(form) {
-    let data = new Contato(
-        form.elements.namedItem("nome").value,
-        form.elements.namedItem("sobrenome").value,
-        form.elements.namedItem("email").value,
-        form.elements.namedItem("cpf").value,
-        form.elements.namedItem("telefone").value,
-        form.elements.namedItem("tipo").value,
-        form.elements.namedItem("mensagem").value,
-    );
+  event.preventDefault();
 
-    console.log(data);
-    Enviar();
+  let data = new Contato(
+    form.elements.namedItem("nome").value,
+    form.elements.namedItem("sobrenome").value,
+    form.elements.namedItem("email").value,
+    form.elements.namedItem("cpf").value,
+    form.elements.namedItem("telefone").value,
+    form.elements.namedItem("tipo").value,
+    form.elements.namedItem("mensagem").value,
+  );
+
+  console.log(data);
+  Enviar();
 }
 
 function Enviar() {
-    let nome = document.getElementById("nome");
-    if (nome.value != "") {
-      alert(`Obrigado sr(a) ${nome.value} os seus dados foram encaminhados com sucesso`);
-    }
+  let nome = document.getElementById("nome");
+  if (nome.value != "") {
+    alert(
+      `Obrigado sr(a) ${nome.value} os seus dados foram encaminhados com sucesso`,
+    );
+  }
 }
